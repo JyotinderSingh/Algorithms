97e1e7531767835c2c3b993ed122dcaa9b516ea1
JyotinderSingh <jyotindrsingh@gmail.com>
2020-02-23 16:26:28 +0530

    added comments

--- EPI/Multiply_Two_Arbitrary_Precision_Integers/MultiplyTwoArbitraryPrecisionIntegers.cpp
+++ EPI/Multiply_Two_Arbitrary_Precision_Integers/MultiplyTwoArbitraryPrecisionIntegers.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> Multiply(vector<int> num1, vector<int> num2)
 {
     int sign = num1.front() < 0 ^ num2.front() < 0 ? -1 : 1;
-    num1.front() = abs(num1.front()), num2.front = abs(num2.front());
+    num1.front() = abs(num1.front()), num2.front() = abs(num2.front());
 
     vector<int> result(num1.size() + num2.size(), 0);
 
@@ -18,6 +18,11 @@ vector<int> Multiply(vector<int> num1, vector<int> num2)
         }
     }
 
+    // NOTE
+    // find_if_not returns the iterator to an element, for which the 
+    // given predicate returns false.
+    // i.e. it slices the array, starting from the first non-zero position
+    // till the end.
     result = vector<int>{find_if_not(begin(result), end(result), [](int a) {
                              return !a;
                          }),
@@ -33,4 +38,10 @@ vector<int> Multiply(vector<int> num1, vector<int> num2)
 // Tester code
 int main()
 {
+    vector<int> num1{1, 2, 3};
+    vector<int> num2{9,8,7};
+    vector<int> res = Multiply(num1, num2);
+    for(auto i: res){
+        cout<<i;
+    }
 }
