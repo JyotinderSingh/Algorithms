b4f21333f65bac3d01a3d216a712267841cbbc87
JyotinderSingh <jyotindrsingh@gmail.com>
2020-04-03 20:51:49 +0530

    max-subarray added

---
+++ LeetCode/maximum-subarray.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-subarray/
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        vector<long long int> dp(nums.size());
+        long long int max_sum = INT_MIN;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            max_sum = nums[i] > (max_sum + nums[i]) ? nums[i] : (max_sum + nums[i]);
+            dp[i] = max_sum;
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
