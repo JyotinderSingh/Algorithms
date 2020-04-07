6c64951f9321a1bf803a21a41b46b8ab1b31f3ec
JyotinderSingh <jyotindrsingh@gmail.com>
2020-04-07 13:00:55 +0530

    minor edit

--- LeetCode/coutning-elements.cpp
+++ LeetCode/coutning-elements.cpp
@@ -1,4 +1,7 @@
 // https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3289/
+// Given an integer array arr, count element x such that x + 1 is also in arr.
+
+// If there're duplicates in arr, count them seperately.
 class Solution
 {
 public:
