d17353c7fdb278c2f838da707adb22cd1a7ebb44
JyotinderSingh <jyotindrsingh@gmail.com>
2020-04-22 16:49:10 +0530

    construct-bst-from-preorder

---
+++ LeetCode/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/submissions/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    int i = 0;
+    TreeNode *bstFromPreorder(vector<int> &A, int bound = INT_MAX)
+    {
+        if (i == A.size() || A[i] >= bound)
+        {
+            return NULL;
+        }
+        TreeNode *root = new TreeNode(A[i++]);
+        root->left = bstFromPreorder(A, root->val);
+        root->right = bstFromPreorder(A, bound);
+        return root;
+    }
+};
