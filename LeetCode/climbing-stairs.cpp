384a536de1ff942b154ce640176fe33f46ff0588
JyotinderSingh <jyotindrsingh@gmail.com>
2020-07-30 13:57:16 +0530

    leetcode

--- LeetCode/climbing-stairs.cpp
+++ LeetCode/climbing-stairs.cpp
@@ -1,4 +1,22 @@
 // https://leetcode.com/problems/climbing-stairs/
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1, dp[1] = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+/////////////////////////////////////////
+
 class Solution
 {
 public:
@@ -15,7 +33,7 @@ public:
                     dp[i] += dp[i - j];
                 }
                 // or simply write
-                // dp[i] = dp[i - 1] + dp[i - 1]
+                // dp[i] = dp[i - 1] + dp[i - 2]
                 // and start outer loop from i = 2
             }
         }
