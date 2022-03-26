55baf8a6f33d230c23e2e49cd021054b90fb6ee3
Jyotinder Singh <jyotindrsingh@gmail.com>
2022-03-27 00:20:38 +0530

    LPSubstring

--- LeetCode/longest-palindromic-substring.java
+++ LeetCode/longest-palindromic-substring.java
@@ -1,8 +1,31 @@
 // https://leetcode.com/problems/longest-palindromic-substring/
 
-// Manacher's algorithm
+// O(N^2) time, O(1) space
 class Solution {
-  
+  public String longestPalindrome(String s) {
+    if (s == null || s.length() < 1)
+      return "";
+    int start = 0, end = 0;
+
+    for (int i = 0; i < s.length(); ++i) {
+      int len1 = expandAroundCenter(s, i, i);
+      int len2 = expandAroundCenter(s, i, i + 1);
+      int len = Math.max(len1, len2);
+      if (len > end - start) {
+        start = i - (len - 1) / 2;
+        end = i + len / 2;
+      }
+    }
+    return s.substring(start, end + 1);
+  }
+
+  public int expandAroundCenter(String s, int left, int right) {
+    while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+  }
 }
 
 // DP O(N^2) time, O(N^2) space
